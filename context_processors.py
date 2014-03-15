24df7938476701a5b1b8bbd51d8ae0547942707a
Amit Roushan <akroushan@gmail.com>
2014-03-15 04:05:19 -0400

    added date default value

--- context_processors.py
+++ context_processors.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.template import RequestContext
+from datetime import datetime
+from django.utils.dateformat import DateFormat
 
 def context_rawBase(request):
     "A context processor that provides all variables used in the base html."
@@ -8,5 +10,6 @@ def context_rawBase(request):
         'ip_address': request.META['REMOTE_ADDR'],
 	'phoneNo' : '9876543210',
 	'emailId' : 'spotmyarena@gmail.com',
+	'todaysDate' : DateFormat(datetime.now()).format('d M Y'),
 	
     }
--- views.py
+++ views.py
@@ -6,23 +6,27 @@ from django.core import serializers
 from spotmyarena.vendorData.models import Sport,Club
 from spotmyarena.context_processors import context_rawBase
 from django.template import RequestContext
+from urlparse import urlparse, parse_qs
+from django.shortcuts import render_to_response
 
 def indexView(request):
-    sport_list = serializers.serialize('python', Sport.objects.all(), fields=('sport_name','sport_id'))
-    return render(request, "index.html", { 'sport_list': sport_list, }, context_instance=RequestContext(request, processors=[context_rawBase]))
-    return HttpResponse(html)
+	    sport_list = serializers.serialize('python', Sport.objects.all(), fields=('sport_name','sport_id'))
+	    return render(request, "index.html", { 'sport_list': sport_list, }, 
+	    context_instance=RequestContext(request, processors=[context_rawBase]))
+	    return HttpResponse(html)
+		
 
 def searchResultsView(request):
-    return render(request, "searchResults.html", context_instance=RequestContext(request, processors=[context_rawBase]))
-    return HttpResponse(html)
-
+    	    return render(request, "searchResults.html", context_instance=RequestContext(request, processors=[context_rawBase]))
+    	    return HttpResponse(html)
+	    
 def clubProfile(request):
-    return render(request, "clubProfile.html", context_instance=RequestContext(request, processors=[context_rawBase]))
-    return HttpResponse(html)
+	    return render(request, "clubProfile.html", context_instance=RequestContext(request, processors=[context_rawBase]))
+	    return HttpResponse(html)
 
 def all_json_areas(request, sportId):
-    current_sport = Sport.objects.get(sport_id=sportId)
-    areas = Club.objects.all().filter(club_sport_id=current_sport)
-    json_areas = serializers.serialize("json", areas)
-    return HttpResponse(json_areas, mimetype="application/javascript")
+	    current_sport = Sport.objects.get(sport_id=sportId)
+	    areas = Club.objects.all().filter(club_sport_id=current_sport)
+	    json_areas = serializers.serialize("json", areas)
+	    return HttpResponse(json_areas, mimetype="application/javascript")
 
